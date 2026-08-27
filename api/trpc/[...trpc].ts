7ae7e16648b00b9823a314eca501dc9cb6626397
trheshamwalidv <heshamwalid9996@gmail.com>
2026-08-27 20:28:24 +0000

    fix: load nested tRPC handler as ESM

--- api/trpc/[...trpc].ts
+++ api/trpc/[...trpc].ts
@@ -1,10 +1,4 @@
-import { createRequire } from "node:module";
-
-
-
-const require = createRequire(import.meta.url);
-
-const app = require("./_handler.cjs").default;
+import app from "./_handler.mjs";
 
 
 
